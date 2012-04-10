3ac90762a4b0538a4462ccd9bb633e7fff32ff52
Stan Hutcheon <stantheripper@gmail.com>
2012-04-10 01:19:49 +0100

    first commit

---
+++ xSID/cmds.vb
@@ -0,0 +1,27 @@
+﻿Module cmds
+
+    Public Sub simpack(ByVal sim As String, ByVal dir As String)
+        ChDir(My.Application.Info.DirectoryPath)
+        Dim args As String = " x " + Chr(34) + sim + Chr(34) + " " + Chr(34) + dir + Chr(34)
+        Dim simpack As Process = New Process
+        simpack.StartInfo.Arguments = args
+        simpack.StartInfo.RedirectStandardOutput = True
+        simpack.StartInfo.UseShellExecute = False
+        simpack.StartInfo.FileName = My.Application.Info.DirectoryPath + "\simpack.exe"
+        simpack.Start()
+        Do Until simpack.HasExited
+            For i = 0 To 5000000
+            Next
+        Loop
+        simpack.WaitForExit()
+    End Sub
+
+    Public Function getSIMName(ByVal dir As String) As String
+        For Each file In System.IO.Directory.GetFiles(dir)
+            If file.EndsWith(".sim") Then
+                getSIMName = file.ToString
+            End If
+        Next
+    End Function
+
+End Module
---
+++ xSID/main.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Windows.Forms
+Module main
+
+    Sub Main()
+        If Not System.IO.File.Exists(My.Application.Info.DirectoryPath + "\setup.ini") Then
+            MsgBox("Please make sure you are running this from the game install root, this contains SteamService.exe and splash.tga", MsgBoxStyle.Exclamation, "xSID - Error")
+        End If
+        Dim ini As String = My.Computer.FileSystem.ReadAllText(My.Application.Info.DirectoryPath + "\setup.ini")
+        Dim game = Split(ini, Chr(34))
+        Dim finalgame = game(3).ToString
+        Dim requiredSize = Val(game(11)) / 1000000
+
+        Console.WriteLine("Game: " + finalgame)
+        Console.WriteLine("required: " + requiredSize.ToString + "GB")
+        Console.WriteLine("")
+
+        Dim folder As FolderBrowserDialog = New FolderBrowserDialog
+        folder.Description = "Please select an install directory for " + finalgame
+        Do Until Not folder.SelectedPath = ""
+            folder.ShowDialog()
+            If folder.SelectedPath = "" Then
+                MsgBox("Please select an install directory", MsgBoxStyle.Exclamation, "xSID - Error")
+            End If
+        Loop
+        Console.WriteLine("Selected Path: " + folder.SelectedPath)
+        Dim drive = System.IO.Directory.GetDirectoryRoot(folder.SelectedPath)
+        Dim driveinfo = My.Computer.FileSystem.GetDriveInfo(drive.ToString)
+        Dim drivefreespace = driveinfo.AvailableFreeSpace / 1000000000
+        Console.WriteLine("Free space: " + drivefreespace.ToString + "GB")
+
+        If drivefreespace < requiredSize Then
+            MsgBox("The directory you selected is too small, please select another one, or free up some space", MsgBoxStyle.Exclamation, "xSID - Error")
+            Exit Sub
+        End If
+
+        simpack(getSIMName(My.Application.Info.DirectoryPath), folder.SelectedPath)
+
+    End Sub
+
+
+End Module
